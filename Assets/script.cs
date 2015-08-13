fd2e59d0c51115b7f1547a2c8257e41f3f496d1c
Carlos Mondragon <carlosmondragon10@gmail.com>
2015-08-13 17:58:38 -0500

    first commit

---
+++ Assets/script.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script : MonoBehaviour {
+
+	public Material entrada;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void NextScene(){
+		Application.LoadLevel("sistemas");
+	}
+	
+	public void NextSceneDos(){
+		Application.LoadLevel("arco");
+	}
+
+	public void ToggleVRMode() {
+		Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
+	}
+}
